6960325ec7e483de5898f3cd4b4b743fa48db57f
Josue Tomalá Esparza <josue_3steban@hotmail.com>
2022-08-06 04:08:37 -0500

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -29,29 +29,29 @@ export const Projects = () => {
 
         <div className="sm:mt-10 sm:px-5 flex flex-col items-center mt-20 mb-20">
 
-            <article className='sm:flex-col-reverse sm:w-full sm:gap-3 sm:p-3  w-3/4 flex gap-5 p-10 border-[#17122A] border-l-2 border-b-2'>
+//             <article className='sm:flex-col-reverse sm:w-full sm:gap-3 sm:p-3  w-3/4 flex gap-5 p-10 border-[#17122A] border-l-2 border-b-2'>
 
-                <div className="sm:w-full  flex flex-col w-1/2 text-[#17122A]">
-                    <div className="sm:grow-0  grow flex flex-col justify-center">
-                        <h2 className='sm:hidden sm:text-lg sm:text-center  mb-5 uppercase text-2xl font-bold'>Registro de <span className='text-[#D01F5A]'>pacientes</span></h2>
-                        <p>Práctica en base a cursos realizados, pero siguiendo una estructura y diseño de gustos personales. Consiste es un formulario para agregar pacientes y a una lista de estos mismos. Hecho con React y Tailwindcss</p>
-                    </div>
+//                 <div className="sm:w-full  flex flex-col w-1/2 text-[#17122A]">
+//                     <div className="sm:grow-0  grow flex flex-col justify-center">
+//                         <h2 className='sm:hidden sm:text-lg sm:text-center  mb-5 uppercase text-2xl font-bold'>Registro de <span className='text-[#D01F5A]'>pacientes</span></h2>
+//                         <p>Práctica en base a cursos realizados, pero siguiendo una estructura y diseño de gustos personales. Consiste es un formulario para agregar pacientes y a una lista de estos mismos. Hecho con React y Tailwindcss</p>
+//                     </div>
 
-                    <div className="sm:justify-between sm:mt-5 sm:text-sm  flex justify-evenly">
-                        <a className=' uppercase block p-2 text-[#D01F5A] border-2 border-[#D01F5A] rounded-md hover:text-white hover:bg-[#D01F5A] transition-all ' href="https://practica-citas-medicas.netlify.app/" target="_blank">ver sitio</a>
-                        <a className='  uppercase block p-2 text-[#D01F5A] border-2 border-[#D01F5A] rounded-md hover:text-white hover:bg-[#D01F5A] transition-all' href="https://github.com/josu3steban/practica-citas" target="_blank">repositorio</a>
-                    </div>
-                </div>
+//                     <div className="sm:justify-between sm:mt-5 sm:text-sm  flex justify-evenly">
+//                         <a className=' uppercase block p-2 text-[#D01F5A] border-2 border-[#D01F5A] rounded-md hover:text-white hover:bg-[#D01F5A] transition-all ' href="https://practica-citas-medicas.netlify.app/" target="_blank">ver sitio</a>
+//                         <a className='  uppercase block p-2 text-[#D01F5A] border-2 border-[#D01F5A] rounded-md hover:text-white hover:bg-[#D01F5A] transition-all' href="https://github.com/josu3steban/practica-citas" target="_blank">repositorio</a>
+//                     </div>
+//                 </div>
 
-                <div className="sm:w-full  w-1/2">
-                    <h2 className='sm:text-lg sm:text-center  mb-5 uppercase text-2xl font-bold md:hidden'>Registro de <span className='text-[#D01F5A]'>pacientes</span></h2>
-                    <div className=" rounded-lg overflow-hidden shadow-lg border-2">
-                        <img src={ ImgPaciente } alt="captura de pacientes" />
-                    </div>
-                </div>
+//                 <div className="sm:w-full  w-1/2">
+//                     <h2 className='sm:text-lg sm:text-center  mb-5 uppercase text-2xl font-bold md:hidden'>Registro de <span className='text-[#D01F5A]'>pacientes</span></h2>
+//                     <div className=" rounded-lg overflow-hidden shadow-lg border-2">
+//                         <img src={ ImgPaciente } alt="captura de pacientes" />
+//                     </div>
+//                 </div>
                 
                 
-            </article>
+//             </article>
 
             <article className='sm:flex-col-reverse sm:w-full sm:gap-3 sm:p-3  mt-20 w-3/4 flex gap-5 p-10 border-[#17122A] border-r-2 border-b-2'>
 
@@ -101,28 +101,28 @@ export const Projects = () => {
 
             </article>
 
-            <article className='sm:flex-col-reverse sm:w-full sm:gap-3 sm:p-3  mt-20 w-3/4 flex gap-5 p-10 border-[#17122A] border-r-2 border-b-2'>
+//             <article className='sm:flex-col-reverse sm:w-full sm:gap-3 sm:p-3  mt-20 w-3/4 flex gap-5 p-10 border-[#17122A] border-r-2 border-b-2'>
 
-                <div className="sm:w-full  flex flex-col w-1/2 text-[#17122A]">
-                    <div className="sm:grow-0  grow flex flex-col justify-center">
-                        <h2 className='sm:hidden  mb-5 uppercase text-2xl font-bold'>administración de <span className='text-[#D01F5A]'>personas</span></h2>
-                        <p>Práctica que consiste en una CRUD de administracón de personas, haciendo uso de json-server, para la simulación de peticiones a una API REST. Hecho con React, Tailwindcss, Redux, Json-Server, Formik y Yup</p>
-                    </div>
+//                 <div className="sm:w-full  flex flex-col w-1/2 text-[#17122A]">
+//                     <div className="sm:grow-0  grow flex flex-col justify-center">
+//                         <h2 className='sm:hidden  mb-5 uppercase text-2xl font-bold'>administración de <span className='text-[#D01F5A]'>personas</span></h2>
+//                         <p>Práctica que consiste en una CRUD de administracón de personas, haciendo uso de json-server, para la simulación de peticiones a una API REST. Hecho con React, Tailwindcss, Redux, Json-Server, Formik y Yup</p>
+//                     </div>
 
-                    <div className="sm:justify-between sm:mt-5 sm:text-sm  flex justify-evenly">
-                        <a className='uppercase block p-2 text-[#D01F5A] border-2 border-[#D01F5A] rounded-md hover:text-white hover:bg-[#D01F5A] transition-all ' href="https://practica-aspirante.netlify.app/" target="_blank">ver sitio</a>
-                        <a className='uppercase block p-2 text-[#D01F5A] border-2 border-[#D01F5A] rounded-md hover:text-white hover:bg-[#D01F5A] transition-all' href="https://github.com/josu3steban/practica-admin-aspirantes" target="_blank">repositorio</a>
-                    </div>
-                </div>
+//                     <div className="sm:justify-between sm:mt-5 sm:text-sm  flex justify-evenly">
+//                         <a className='uppercase block p-2 text-[#D01F5A] border-2 border-[#D01F5A] rounded-md hover:text-white hover:bg-[#D01F5A] transition-all ' href="https://practica-aspirante.netlify.app/" target="_blank">ver sitio</a>
+//                         <a className='uppercase block p-2 text-[#D01F5A] border-2 border-[#D01F5A] rounded-md hover:text-white hover:bg-[#D01F5A] transition-all' href="https://github.com/josu3steban/practica-admin-aspirantes" target="_blank">repositorio</a>
+//                     </div>
+//                 </div>
 
-                <div className="sm:w-full  w-1/2">
-                    <h2 className='sm:text-lg sm:text-center  mb-5 uppercase text-2xl font-bold md:hidden'>administración de <span className='text-[#D01F5A]'>personas</span></h2>
-                    <div className="rounded-lg overflow-hidden shadow-lg border">
-                        <img src={ ImgAdmin } alt="captura de administracion de personas" />
-                    </div>
-                </div>
+//                 <div className="sm:w-full  w-1/2">
+//                     <h2 className='sm:text-lg sm:text-center  mb-5 uppercase text-2xl font-bold md:hidden'>administración de <span className='text-[#D01F5A]'>personas</span></h2>
+//                     <div className="rounded-lg overflow-hidden shadow-lg border">
+//                         <img src={ ImgAdmin } alt="captura de administracion de personas" />
+//                     </div>
+//                 </div>
                 
-            </article>
+//             </article>
   
             <article className='sm:flex-col-reverse sm:w-full sm:gap-3 sm:p-3  mt-20 w-3/4 flex gap-5 p-10 border-[#17122A] border-l-2 border-b-2'>
 
