3bba3a741024f7b347204d47ac4b55bb659f6a2f
Josue Tomalá Esparza <josue_3steban@hotmail.com>
2022-08-06 04:22:00 -0500

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,6 +3,7 @@ import ImgControl from '../assets/img/pract-control.jpeg';
 import ImgCripto from '../assets/img/pract-cripto.jpeg';
 import ImgAdmin from '../assets/img/pract-admin.jpeg';
 import ImgNotes from '../assets/img/notas.png';
+import ImgAdminProjects from '../assets/img/adminProject.png';
 
 export const Projects = () => {
   return (
@@ -78,11 +79,10 @@ export const Projects = () => {
             </article>
   
             <article className='sm:flex-col-reverse sm:w-full sm:gap-3 sm:p-3  mt-20 w-3/4 flex gap-5 p-10 border-[#17122A] border-l-2 border-b-2'>
-
                 <div className="sm:w-full  flex flex-col w-1/2 text-[#17122A]">
                     <div className="sm:grow-0  grow flex flex-col justify-center">
                         <h2 className='sm:hidden  mb-5 uppercase text-2xl font-bold'>aplicación de <span className='text-[#D01F5A]'>notas</span></h2>
-                        <p>Práctica de un MERN de aplicación de notas, construida desde el back-end hasta el front-end, en la que para hacer uso de esta el que se deberá registrarse e iniciar sesión, siendo esta contralada por tokens. Hecha con React, Tailwindcss, Redux, Node.js, Express, JWT, MongoDB, Mongoose, Formik, Yup.</p>
+                        <p>Práctica de un MERN de aplicación de notas, construida desde el back-end hasta el front-end, en la que para hacer uso de esta el que se deberá registrarse e iniciar sesión, siendo esta contralada por tokens. Hecha con React, Tailwindcss, Redux, Node.js, Express, JWT, MongoDB, Formik.</p>
                     </div>
 
                     <div className="sm:justify-between sm:mt-5 sm:text-sm  flex justify-evenly">
@@ -96,10 +96,28 @@ export const Projects = () => {
                     <div className="rounded-lg overflow-hidden shadow-lg border">
                         <img src={ ImgNotes } alt="captura de aplicación de notas" />
                     </div>
+                </div>   
+            </article>
+  
+            <article className='sm:flex-col-reverse sm:w-full sm:gap-3 sm:p-3  mt-20 w-3/4 flex gap-5 p-10 border-[#17122A] border-l-2 border-b-2'>
+                <div className="sm:w-full  flex flex-col w-1/2 text-[#17122A]">
+                    <div className="sm:grow-0  grow flex flex-col justify-center">
+                        <h2 className='sm:hidden  mb-5 uppercase text-2xl font-bold'>aplicación de <span className='text-[#D01F5A]'>administración de proyectos</span></h2>
+                        <p>Proyecto MERN de una aplicación de administración de proyectos, con registro obligatorio y confirmación de cuenta mediante correo electrónico, añadiendo la funcionalidad de poder crear una nueva contraseña en caso de olvidarla. Dentro de la app se podrá crear proyectos y a este se le podrá añadir tareas y asi mismo también colaboradores para que ayuden en el cumplimiento de dichas tareas Hecha con React, Tailwindcss, Redux, Node.js, Express, JWT, MongoDB, Formik.</p>
+                    </div>
+
+                    <div className="sm:justify-between sm:mt-5 sm:text-sm  flex justify-evenly">
+                        <a className='uppercase block p-2 text-[#D01F5A] border-2 border-[#D01F5A] rounded-md hover:text-white hover:bg-[#D01F5A] transition-all ' href="https://taskmanager-jete.netlify.app/" target="_blank">ver sitio</a>
+                        <a className='uppercase block p-2 text-[#D01F5A] border-2 border-[#D01F5A] rounded-md hover:text-white hover:bg-[#D01F5A] transition-all' href="https://github.com/josu3steban/practica-notas" target="_blank">repositorio</a>
+                    </div>
                 </div>
 
-                
-                
+                <div className="sm:w-full  w-1/2">
+                    <h2 className='sm:text-lg sm:text-center  mb-5 uppercase text-2xl font-bold md:hidden'>aplicación de <span className='text-[#D01F5A]'>administración de proyectos</span></h2>
+                    <div className="rounded-lg overflow-hidden shadow-lg border">
+                        <img src={ ImgAdminProjects } alt="captura de aplicación de administración de proyectos" />
+                    </div>
+                </div>   
             </article>
 
         </div>
